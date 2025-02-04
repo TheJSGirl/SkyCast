9d922513042e2c90c6cbc524b24c8a17d761feab
Priyanka <prinegi100@gmail.com>
2025-02-04 16:05:10 +0530

    corrected proptype for forecastListItem

--- app/components/ForecastListItem/index.tsx
+++ app/components/ForecastListItem/index.tsx
@@ -2,7 +2,16 @@ import { View, Image } from 'react-native';
 import styles from './forecastListItem.style';
 import Txt from '../Txt';
 
-export default function Forecasts({image, day, date, temperature}) {
+interface propType  {
+    image: string,
+    day: number,
+    date: number,
+    temperature: string
+
+
+}
+
+export default function Forecasts({image, day, date, temperature}:propType ) {
 
     return <View style={styles.container}>
         <Image  source={image}  style={styles.image}/>
